d6d521e8926f7ec566e760a11f84bfb5db18745f
Sanjaya <sanjaya.w@jinasena.com.lk>
2026-09-20 16:34:05 +0530

    Initial: master-data extracted from CDB (2026-09-20)

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Jinasena : MasterData : Stock',
-    'version': '17.0.0.0.1',
+    'version': '17.0.0.0.2',
     'summary': 'Master-data extracted from CDB for Stock domain.',
     'description': 'Extracted from Clear-DB. Test-env master data. Edit the CSVs in data/ to add/remove rows before install.',
     'author': 'Jinasena Agricultural Machinery (Pvt) Ltd.',
